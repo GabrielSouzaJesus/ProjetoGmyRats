d52fbd76a8bba3daded72a456ee2d5e69de1750c
Gabriel Souza Jesus <gabriel-itabuna@hotmail.com>
2025-07-25 01:44:25 -0300

    Renomeia arquivos de animação para nomes sem acento e ajusta imports

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
-import footballPractice from "../../animacoes/Prática de Futebol.json";
-import sport from "../../animacoes/Esporte.json";
-import Exerciseanimation from "../../animacoes/Animação de exercício.json";
+import footballPractice from "../../animacoes/pratica-de-futebol.json";
+import sport from "../../animacoes/esporte.json";
+import Exerciseanimation from "../../animacoes/animacao-de-exercicio.json";
 
 export default function SplashScreen({ onFinish }) {
   const [showPageMsg, setShowPageMsg] = useState(true);
